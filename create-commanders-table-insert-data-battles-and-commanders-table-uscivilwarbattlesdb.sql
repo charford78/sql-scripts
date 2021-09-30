4ec1d881a70ef9dab7c6d64fa0f4d99a1f9531be
Charlie Ford <charles.ford1978@gmail.com>
2021-09-30 16:37:56 -0400

    Removed unnecessary code from the Commander table creation script and made some adjustments to the create commanders table query file

--- create-commanders-table-insert-data-battles-and-commanders-table-uscivilwarbattlesdb.sql
+++ create-commanders-table-insert-data-battles-and-commanders-table-uscivilwarbattlesdb.sql
@@ -4,9 +4,23 @@ insert Battles (BattleCode, BattleName, Location, Theatre, Victor)
 values ('WILCRK', 'Battle of Wilson''s Creek', 'Wilson''s Creek, MO',
 	'Western', 'CSA');
 
+update Battles set
+	Date = 'August 10th, 1861'
+	where BattleCode = 'WILCRK';
+
+alter table Battles
+	alter column Location varchar(40);
+
+alter table battles
+	alter column Theatre varchar(40);
+
+update Battles set
+	Theatre = 'Trans-Mississippi'
+		where BattleCode = 'WILCRK';
+
 select * from Commanders;
 
-insert Commanders (Name, Army, BattleId)
+insert Commanders (Commander, Army, BattleId)
 	values
 		('P.G.T. Beauregard', 'CSA', (Select Id from Battles 
 									where BattleCode = '1BRUN')),
@@ -31,3 +45,4 @@ insert Commanders (Commander, Army, BattleId)
 		('Samuel D. Sturgis', 'USA', (select Id from Battles
 								where BattleCode = 'WILCRK'));
 
+	
--- script-to-create-Commanders-table-uscivilwarbattlesdb.sql
+++ script-to-create-Commanders-table-uscivilwarbattlesdb.sql
@@ -1,28 +1,13 @@
 USE [USCivilWarBattles]
 GO
-
-/****** Object:  Table [dbo].[Commanders]    Script Date: 9/30/2021 1:26:19 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
 DROP TABLE Commanders
 GO
 CREATE TABLE [dbo].[Commanders](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL primary key,
 	[Commander] [varchar](30) NOT NULL,
 	[Army] [varchar](10) NOT NULL,
-	[BattleId] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Commanders]  WITH CHECK ADD FOREIGN KEY([BattleId])
-REFERENCES [dbo].[Battles] ([Id])
+	[BattleId] [int] NOT NULL foreign key references Battles(Id)
+)
 GO
 
 
